937f267443f2c56d5fd19156b3b8ceeeadda0f51
betsystevens <betsystevens5@gmail.com>
2021-03-26 20:07:30 -0500

    refactor inner and outer layout templates

--- classes/Template.php
+++ classes/Template.php
@@ -4,7 +4,7 @@ class Template {
   // properties
   protected $template, $data;
   private $_path = __DIR__ . '/../templates/';
-  private $baseTemplate = __DIR__ . '/../templates/layout.html.php';
+  private $outerLayout = __DIR__ . '/../templates/layout.html.php';
 
   // constructor
   public function __construct($filename, $data = array()) {
@@ -12,27 +12,27 @@ class Template {
     $this->data = $data;
   }
 
-  public function render() {
+  // inner content
+  public function renderInner() {
     if(file_exists($this->template)){
         //Extracts vars to current view scope
         extract($this->data);
-
-        //Starts output buffering
         ob_start();
-
         //Includes contents
         include $this->template;
         $output = ob_get_clean();
-        
-        ob_start();
-        include $this->baseTemplate;
-        $buffer = ob_get_clean();
-        //Returns output buffer
-        return $buffer;
-
+        return $output;
     } else {
        throw new Exception('template file does not exist');
     }
+  }
+  public function render() {
+    $output = $this->renderInner();
+    $title = $this->data['title'];
+    ob_start();
+    include $this->outerLayout;
+    $buffer = ob_get_clean();
+    return $buffer;
   }
 }       
 
