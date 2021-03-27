56fb63aa585140350d1680fe719e8a7e28bb6f59
betsystevens <betsystevens5@gmail.com>
2021-03-27 11:09:25 -0500

    add styles to report

--- templates/scoutTotals.html.php
+++ templates/scoutTotals.html.php
@@ -1,9 +1,10 @@
 <h2><?= $title ?></h2>
 <?php foreach ($totalsRecords as $scoutRecord): ?>
-  <div class="grid four-col">
-    <div><?=$scoutRecord['ID']?></div>
+  <div class="grid five-col">
+    <div class="right"><?=$scoutRecord['ID']?></div>
+    <div></div>
     <div><?=$scoutRecord['Scout']?></div>
-    <div><?=$scoutRecord['Total']?></div>
-    <div><?=$scoutRecord['Orders']?></div>
+    <div class="right"><?='$'.$scoutRecord['Total']?></div>
+    <div class="right"><?=$scoutRecord['Orders']?></div>
 </div>
 <?php endforeach; ?>
