a6b96e8b8bafd29c0ef07c685f0761a5092fe3ba
betsystevens <betsystevens5@gmail.com>
2021-03-29 12:31:21 -0500

    fix styles, refactor

--- templates/scouts.html.php
+++ templates/scouts.html.php
@@ -5,13 +5,13 @@
 		<div>
 			<a class="btn" href="editScout.php?id=<?=$scout['scoutid']?>">edit</a>
 		</div>
-		<div class="field">	
+		<div class="self-start">	
 			<?=htmlspecialchars($scout['lastname'], ENT_QUOTES, 'UTF-8') . ", "?>
 			<?=htmlspecialchars($scout['firstname'], ENT_QUOTES, 'UTF-8')?>
 		</div>
-		<form class="justify-end" action="deleteScout.php" method="post">
+		<form class="justify-self-end" action="deleteScout.php" method="post">
 				<input type="hidden" name="id" value=<?=$scout['scoutid']?>>
-				<input class="btn" type="submit" value="delete">
+				<input class="btn justify-self-end" type="submit" value="delete">
 		</form>
 	<?php endforeach; ?>		
 </div>
