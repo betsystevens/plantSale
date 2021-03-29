459e4ea0c69383379273c195fd42820af099e14a
betsystevens <betsystevens5@gmail.com>
2021-03-29 13:47:42 -0500

    add and fix styles

--- templates/selectScout.html.php
+++ templates/selectScout.html.php
@@ -1,10 +1,10 @@
-<form method="post" action="">
-  <h2><?= $title ?></h2>
-  <div class="select container">
-    <label for="scout">Choose a Scout</label>
+<h2><?= $title ?></h2>
+<form class="h-half" method="post" action="">
+  <div class="flex flex-col space-around h-third">
+    <label for="scout" class="font-semi">Choose a Scout</label>
     <select id="scout" name="scoutid">
       <?php foreach($scouts as $scout): ?>
-        <option value=<?=$scout['scoutid']?>>
+        <option class="" value=<?=$scout['scoutid']?>>
           <?= $scout['lastname'].', '.$scout['firstname'] ?>
         </option>
       <?php endforeach; ?>
