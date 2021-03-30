f8277c05b2636126166a1fc9741bed3b3536c93b
betsystevens <betsystevens5@gmail.com>
2021-03-29 19:53:11 -0500

    add query for one scouts orders to database class

--- classes/DatabaseTable.php
+++ classes/DatabaseTable.php
@@ -38,4 +38,26 @@ class DatabaseTable {
 		$query = $this->query($sql);
 		return $query->fetchAll(PDO::FETCH_ASSOC);
 	}
+	public function oneScoutsOrders($pdo, $sid) {
+		$sql = 
+			'SELECT concat(c.lastname,", ", c.firstname) as "customer",
+			o.oid as "order",
+			qty, fname as "flower",
+			fvariety as "variety", fcontainer as "container"
+			FROM flower f
+			INNER JOIN ordflowers of
+					ON f.flowerid = of.flowerid
+			INNER JOIN orders o 
+				ON of.orderid = o.oid
+			INNER JOIN customer c
+				 ON o.cid = c.custid
+			INNER JOIN scout s
+				ON o.sid = s.scoutid AND
+				s.scoutid = :sid 
+			GROUP BY Customer, Container, Flower,Variety
+			ORDER BY Customer, o.oid, Container DESC, Flower, Variety';
+			$parameters = [ ':sid' => $sid ];				
+			$result = $this->query($sql, $parameters);
+			return $result->fetchAll(PDO::FETCH_GROUP);	
+	}
 }
