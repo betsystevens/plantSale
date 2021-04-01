dbdeee01e0318dcd83526eee0cf7e8a0230d204f
betsystevens <betsystevens5@gmail.com>
2021-03-31 23:18:56 -0500

    add sass, style scout order report

--- templates/scoutOrders.html.php
+++ templates/scoutOrders.html.php
@@ -1,29 +1,28 @@
-<div>
-<h2>
-  <a href=<?='exportFile.php?report=scoutOrders&scoutId='.$selectedScout ?>>( &#x2193 )</a>
-  <?= $title ?>
-  <!-- <button>click</button> -->
-</h2>
-</div>
-<form method="GET" action="">
-    <select id="scout" name="scoutid" onchange="this.form.submit()">
-      <?php foreach($scouts as $scout): ?>
-        <option value=<?=$scout['scoutid']?>
-          <?php if($scout['scoutid'] == $selectedScout) : ?>
-            selected
-          <?php endif; ?> >
-          <?= $scout['lastname'].', '.$scout['firstname'] ?>
-        </option>
-      <?php endforeach; ?>
-    </select>
-</form>
+<!-- <div class="scout-orders flex flex-col"> -->
+<div class="scout-orders">
+  <h2 class="flex space-between scout-orders">
+    <?= $title ?>
+    <a href=<?='exportFile.php?report=scoutOrders&scoutId='.$selectedScout ?>> download </a>
+  </h2>
+  <form method="GET" action="">
+      <select id="scout" name="scoutid" onchange="this.form.submit()">
+        <?php foreach($scouts as $scout): ?>
+          <option value=<?=$scout['scoutid']?>
+            <?php if($scout['scoutid'] == $selectedScout) : ?>
+              selected
+            <?php endif; ?> >
+            <?= $scout['lastname'].', '.$scout['firstname'] ?>
+          </option>
+        <?php endforeach; ?>
+      </select>
+  </form>
 
 <h2><?= 'Total Orders: '.$count ?></h2>
 	<?php foreach ($orders as $customer => $order): ?>
     <br />
     <h4><?= $customer ?><h4>
     <?php foreach ($order as $item): ?>
-	  	<div class="grid five-col">
+	  	<div class="grid">
         <div class="qty"><?= $item['qty'] ?></div>
         <div class="fname"><?= $item['flower'] ?></div>
         <div><?= $item['variety'] ?></div>
@@ -31,3 +30,4 @@
 		  </div>
 	  <?php endforeach; ?>
 	<?php endforeach; ?>
+</div>
