59c70eb3722ef7c02ddfae4c38249938904af5b4
betsystevens <betsystevens5@gmail.com>
2021-05-03 22:36:37 -0500

    add groupBy to download button

--- templates/selectScoutGroupBy.html.php
+++ templates/selectScoutGroupBy.html.php
@@ -1,6 +1,6 @@
   <h2 class="flex space-between scout-orders">
     <?= $title ?>
-    <a href=<?='exportFile.php?report=scoutOrders&scoutId='.$selectedScout ?>> download </a>
+    <a href=<?='exportFile.php?report=scoutOrders&scoutId='.$selectedScout.'&groupBy='.$groupBy ?>> download </a>
   </h2>
   <form method="GET" action="">
       <select id="scout" name="scoutid" onchange="this.form.submit()">
@@ -14,14 +14,14 @@
         <?php endforeach; ?>
       </select>
       <select id="groupBy" name="groupBy" onchange="this.form.submit()">
-        <option value=<?= "flower" ?> 
-          <?php if($groupBy == "flower") : ?>
-            selected
-          <?php endif; ?> > by flower </option>
         <option value=<?= "customer" ?>
           <?php if($groupBy == "customer") : ?>
             selected
           <?php endif; ?> > by customer </option>
         </option>
+        <option value=<?= "flower" ?> 
+          <?php if($groupBy == "flower") : ?>
+            selected
+          <?php endif; ?> > by flower </option>
       </select>
   </form>
